ed089591997e7fa69e3547953b71b1dcbcd9ceeb
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-02-13 15:38:14 +1000

    added nix tooling

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A flake for tfsort.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ gomod2nix.overlays.default ];
+            config.allowUnfree = true;
+          };
+
+        in
+        {
+          packages.default = pkgs.callPackage ./. { };
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        })
+    );
+}
